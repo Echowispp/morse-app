c467ddf16f6ad23ef202c9666743ad0d38e459f7
Echowispp <kauri.sykari190@gmail.com>
2025-10-19 23:40:25 +0300

    Final commit, finished project
    Changes:
    README,
    removed buggy highlighting system,
    removed Lorem Ipsum from the start of the page,
    cleaned up code, removing commented-out old code and such,
    fixed the bugs of the highlighting (in the earlier version, it was pretty bad for all characters, not just invalid ones)

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# morse-app
+# Morse translator
+
+This is a simple program that translates morse code to "normal" characters - so dots and dashes into letters, numbers and punctuation. I feel I should also mention I made this for
+
+**Link**: https://morse-app-sand.vercel.app/
+
+# A summary of how this was made:
+
+First, I thought to make a good-looking UI and that took... a while.
+After I had a functional UI, I set on to get some basic functionality in. First I added a 'dictionary' for the code to use to translate morse. So, if there's any mistakes in the translation, you can blame a typo there.
+With the library complete, I set on to finish a way for the boxes to sync, when that was done I put in the rest of the code in commit #3 or #4, depending on whether you count the initial commit. Then, turns out, the system to highlight unsupported characters was really hard to fix and I couldn't do much of anything to fix it. Then, I decided to just leave it be and drop the highlights; currently the page only shows which kind of characters are not translatable but not where they are, unfortunately. And that's pretty much where this project's at right now.
--- main.js
+++ main.js
@@ -86,65 +86,38 @@ function morseToLatin(morse) {
 		.join(" ");
 }
 
-function validateLatin(text) {
-	const isStringValid = /^[a-zåäö!?/=:,._'()0-9\s]*$/i;
-	console.log(isStringValid.test(text));
-	return isStringValid.test(text);
-}
-
-function validateMorse(text) {
-	const isMorseStringValid = /^[.\-\/\s]*$/;
-	return isMorseStringValid.test(text);
-}
-function highlightInvalidChars(box, isLatin) {
+function checkInvalidChars(box, isLatin) {
 	const text = box.textContent;
-	const html = [];
 	let invalidChars = [];
+
 	for (let char of text) {
 		if (isLatin) {
 			if (!/[a-zåäö!?/=:,._'()0-9\s]/i.test(char)) {
-				html.push(
-					`<span style="background-color: #ff6b6b; color: #ffffff">${char}</span>`
-				);
 				invalidChars.push(char);
-			} else {
-				html.push(char);
 			}
 		} else if (!/[.\-\/\s]/.test(char)) {
-			html.push(
-				`<span style="background-color: #ff6b6b; color: #ffffff">${char}</span>`
-			);
 			invalidChars.push(char);
-		} else {
-			html.push(char);
 		}
 	}
-	// return;
-	box.innerHTML = html.join("");
-	// box.innerHTML = "<h1>otsikko</h1><p>tekstiii</p>";
+
 	return invalidChars;
 }
-
 morseBox.addEventListener("input", () => {
-	// return;
 	const morseText = morseBox.textContent.trim();
-	console.log("morseBox addEventListener was called");
-
 	if (!morseText) {
 		latinBox.textContent = "";
 		return;
 	}
 
-	const morseHighlightedChars = highlightInvalidChars(morseBox, false);
+	const morseInvalidChars = checkInvalidChars(morseBox, false);
 
-	// if (morseHighlightedChars.length > 0) {
-	// 	latinBox.textContent =
-	// 		"Invalid morse characters: " +
-	// 		[...new Set(morseHighlightedChars)].join(", ");
-	// }
+	if (morseInvalidChars.length > 0) {
+		latinBox.textContent =
+			"Invalid morse characters: " + [...new Set(morseInvalidChars)].join(", ");
+		return;
+	}
 
 	const latinTranslation = morseToLatin(morseText);
-
 	if (latinTranslation.includes("null")) {
 		latinBox.textContent = "Invalid morse sequence";
 	} else {
@@ -153,32 +126,20 @@ morseBox.addEventListener("input", () => {
 });
 
 latinBox.addEventListener("input", () => {
-	console.log("latin box edit");
-
 	const latinText = latinBox.textContent.trim();
-
 	if (!latinText) {
 		morseBox.textContent = "";
 		return;
 	}
 
-	const latinHighlightedChars = highlightInvalidChars(latinBox, true);
+	const latinInvalidChars = checkInvalidChars(latinBox, true);
 
-	if (latinHighlightedChars.length > 0) {
+	if (latinInvalidChars.length > 0) {
 		morseBox.textContent =
-			"Invalid Latin characters: " +
-			[...new Set(latinHighlightedChars)].join(", ");
+			"Invalid Latin characters: " + [...new Set(latinInvalidChars)].join(", ");
 		return;
 	}
 
 	const morseTranslation = latinToMorse(latinText);
 	morseBox.textContent = morseTranslation;
-});
-
-[morseBox, latinBox].forEach((box) => {
-	box.addEventListener("keydown", (e) => {
-		if (e.key === "Enter") {
-			e.preventDefault();
-		}
-	});
 });
